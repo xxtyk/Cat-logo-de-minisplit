e78d151bb2f654d77d1124ffe9c2de0699685514
xxtyk <htolosarodriguez@gmail.com>
2026-07-24 23:39:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 alert("Catálogo de Minisplits iniciado");
+const minisplits = [
+{
+nombre:"Mirage 1 Tonelada",
+precio:"$4,600",
+foto:"https://via.placeholder.com/600x400?text=Mirage"
+}
+];
+
+const contenedor=document.getElementById("productos");
+
+minisplits.forEach(item=>{
+contenedor.innerHTML+=`
+<div class="card">
+<img src="${item.foto}">
+<h2>${item.nombre}</h2>
+<p class="precio">${item.precio}</p>
+<a class="boton" href="#">Pedir</a>
+</div>
+`;
+});
