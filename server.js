52a8a91194585f3e23b727ac54f4fffd69c48975
xxtyk <htolosarodriguez@gmail.com>
2026-07-26 01:36:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor iniciado en el puerto " + PORT);
+});
