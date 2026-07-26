40c626eb2314db2303a82ad76d983a43ec74965e
xxtyk <htolosarodriguez@gmail.com>
2026-07-25 23:27:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,64 @@
-alert("Catálogo de Minisplits iniciado");
-const minisplits = [
-{
-nombre:"Mirage 1 Tonelada",
-precio:"$4,600",
-foto:"https://via.placeholder.com/600x400?text=Mirage"
+let minisplits = [];
+
+function agregar(){
+
+const nombre = document.getElementById("nombre").value;
+const marca = document.getElementById("marca").value;
+const toneladas = document.getElementById("toneladas").value;
+const voltaje = document.getElementById("voltaje").value;
+const precio = document.getElementById("precio").value;
+const descripcion = document.getElementById("descripcion").value;
+const foto = document.getElementById("foto").value;
+
+minisplits.push({
+nombre,
+marca,
+toneladas,
+voltaje,
+precio,
+descripcion,
+foto
+});
+
+mostrar();
+
+document.getElementById("nombre").value="";
+document.getElementById("marca").value="";
+document.getElementById("toneladas").value="";
+document.getElementById("voltaje").value="";
+document.getElementById("precio").value="";
+document.getElementById("descripcion").value="";
+document.getElementById("foto").value="";
 }
-];
+
+function mostrar(){
 
 const contenedor=document.getElementById("productos");
 
+contenedor.innerHTML="";
+
 minisplits.forEach(item=>{
+
 contenedor.innerHTML+=`
 <div class="card">
-<img src="${item.foto}">
+
+<img src="${item.foto}" width="100%">
+
 <h2>${item.nombre}</h2>
-<p class="precio">${item.precio}</p>
-<a class="boton" href="#">Pedir</a>
+
+<p><b>Marca:</b> ${item.marca}</p>
+
+<p><b>Toneladas:</b> ${item.toneladas}</p>
+
+<p><b>Voltaje:</b> ${item.voltaje}</p>
+
+<p>${item.descripcion}</p>
+
+<h3>${item.precio}</h3>
+
 </div>
 `;
+
 });
+
+}
