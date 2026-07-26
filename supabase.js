897de6a04f32c77ef824f425c0813d201985b073
xxtyk <htolosarodriguez@gmail.com>
2026-07-26 01:39:48 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,10 @@
 
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+module.exports = supabase;
