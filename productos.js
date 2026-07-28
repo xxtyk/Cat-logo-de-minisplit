5f1566da18b2fdf75e48f7df2d5725cafb570468
xxtyk <htolosarodriguez@gmail.com>
2026-07-28 02:53:35 -0700

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,1 +1,17 @@
 
+let productos = [
+{
+id:1,
+nombre:"Mirage X",
+marca:"Mirage",
+toneladas:"1 Tonelada",
+voltaje:"220V",
+precio:5700,
+descripcion:"Minisplit inverter de alta eficiencia.",
+fotos:[
+"https://via.placeholder.com/800x500",
+"https://via.placeholder.com/800x500",
+"https://via.placeholder.com/800x500"
+]
+}
+];
