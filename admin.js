6db7de527e24d1a1f2106ead2f9cb50266e673b0
xxtyk <htolosarodriguez@gmail.com>
2026-07-27 23:49:54 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,39 @@
-
 const supabase = window.supabase.createClient(
   "https://fsadwndjjmpiuoqoahle.supabase.co",
   "sb_publishable_AHjOSgFVDaaXaWl6dR_icA_2lQfhgLu"
 );
 
-function agregar() {
-  alert("Panel conectado correctamente");
+async function agregar() {
+
+  const nombre = document.getElementById("nombre").value;
+  const marca = document.getElementById("marca").value;
+  const toneladas = document.getElementById("toneladas").value;
+  const voltaje = document.getElementById("voltaje").value;
+  const precio = document.getElementById("precio").value;
+  const descripcion = document.getElementById("descripcion").value;
+
+  const { error } = await supabase
+    .from("minisplits")
+    .insert([{
+      nombre,
+      marca,
+      toneladas,
+      voltaje,
+      precio,
+      descripcion
+    }]);
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Minisplit guardado correctamente");
+
+  document.getElementById("nombre").value = "";
+  document.getElementById("marca").value = "";
+  document.getElementById("toneladas").value = "";
+  document.getElementById("voltaje").value = "";
+  document.getElementById("precio").value = "";
+  document.getElementById("descripcion").value = "";
 }
