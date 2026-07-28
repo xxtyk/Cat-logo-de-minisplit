a2aa175c256cc926a90c5c97bc064ec5776b37f9
xxtyk <htolosarodriguez@gmail.com>
2026-07-27 23:14:09 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,9 @@
 
+const supabase = window.supabase.createClient(
+  "https://fsadwndjjmpiuoqoahle.supabase.co",
+  "sb_publishable_AHjOSgFVDaaXaWl6dR_icA_2lQfhgLu"
+);
+
+function agregar() {
+  alert("Panel conectado correctamente");
+}
